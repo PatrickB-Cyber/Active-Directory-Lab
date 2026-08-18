b56e7d814cb0827f2dff3ddab8896f6ce289d10c
Patrick Brennan <patrkb@gmail.com>
2026-08-18 11:31:20 -0400

    Update ad.ps1

--- ad.ps1
+++ ad.ps1
@@ -1,10 +1,16 @@
-##Powershell Commands for Active Directory Lab
+############Powershell Commands for Active Directory Lab
+############This document contains many of the PowerShell and windows Commands used when building, administering and troubleshooting the AD lab
 
-This document contains many of the PowerShell and windows Commands used when building, administering and troubleshooting the AD lab
-
-###View Organizational Units
-
+#View Organizational Units
 Get-ADOrganizationalUnit -Filter * | Select-Object Name, DistinguishedName
 
-###Create an Organizational Unit
+#Create an Organizational Unit
+New-ADOrganizationalUnit -Name "Finance" -Path "OU=Employees,DC=ricky,DC=local"
+
+#Delete an OU 
+#Keep in mind that you may need to disable the accidental deletion setting in order to use this
+Remove-ADOrganizationalUnit -Identity "OU=Finance_Old,OU=Employees,DC=ricky,DC=local"
+
+
+#########User Management
 
