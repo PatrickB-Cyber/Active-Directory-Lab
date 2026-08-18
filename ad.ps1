9295218779aff530a6f833a210db84950b5fe5a2
Patrick Brennan <patrkb@gmail.com>
2026-08-18 11:51:32 -0400

    Update ad.ps1

--- ad.ps1
+++ ad.ps1
@@ -11,6 +11,54 @@ New-ADOrganizationalUnit -Name "Finance" -Path "OU=Employees,DC=ricky,DC=local"
 #Keep in mind that you may need to disable the accidental deletion setting in order to use this
 Remove-ADOrganizationalUnit -Identity "OU=Finance_Old,OU=Employees,DC=ricky,DC=local"
 
-
 #########User Management
+
+#View Domain Users
+Get-ADUser -Filter *
+
+#Find a specific user
+Get-ADUser -Identity jlowpez
+
+#Create a new user
+New-ADUser `
+    -Name "John Doe" `
+    -GivenName "John" `
+    -Surname "Doe" `
+    -SamAccountName "jdoe" `
+    -UserPrincipalName "jdoe@ricky.local" `
+    -Title "Sales Representative" `
+    -Path "OU=Finance,OU=Employees,DC=ricky,DC=local" `
+    -AccountPassword (Read-Host -AsSecureString "FakestPasswordAround" `
+    -Enabled $true `
+    -ChangePasswordAtLogon $true
+
+##########Security Group Management
+
+#View Security Groups
+Get-ADGroup -Filter *
+
+#View Group Members
+Get-ADGroupMember -Identity "group"
+
+#Add User to Security Group
+Add-ADGroupMember -Identity "group" -Members "jdoe"
+
+#Remove User to Security Group
+Remove-ADGroupMember -Identity "group" -Members "jdoe"
+
+# Force Group Policy Update & View updated GPs
+gpupdate /force
+gpresult /r
+
+########Network shares
+
+#View all Shares
+net share
+
+#Manually Map a network drive
+net use A: \\DC-NAME\Finance
+
+#Remove a mapped drive
+net use A: /delete
+
 
